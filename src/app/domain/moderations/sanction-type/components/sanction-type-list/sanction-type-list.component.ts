9aeb14629d5496116a4ed3dc8cb18db825eb29aa
lgaufin <lucas@magnetico.dev>
2024-10-19 11:45:47 -0300

    estructura

---
+++ src/app/domain/moderations/sanction-type/components/sanction-type-list/sanction-type-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sanction-type-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './sanction-type-list.component.html',
+  styleUrl: './sanction-type-list.component.scss'
+})
+export class SanctionTypeListComponent {
+
+}
