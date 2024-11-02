662f795322827082619168ba17a6152c9b697ff2
“114230-RAPACCIONI” <114230@tecnicatura.frc.utn.edu.ar>
2024-11-02 13:08:07 -0300

    Update construction-wrapper.component.ts

--- src/app/domain/construction/components/construction-wrapper/construction-wrapper.component.ts
+++ src/app/domain/construction/components/construction-wrapper/construction-wrapper.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { ConstructionListComponent } from "../construction-list/construction-list.component";
-import { MainContainerComponent } from '../../../../shared/components/main-container/main-container.component';
 
 @Component({
   selector: 'app-construction-wrapper',
   standalone: true,
-  imports: [MainContainerComponent, ConstructionListComponent],
+  imports: [ConstructionListComponent],
   templateUrl: './construction-wrapper.component.html',
   styleUrl: './construction-wrapper.component.css'
 })
