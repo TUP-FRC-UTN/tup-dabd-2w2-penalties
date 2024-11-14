7ff33bc9c2a8de18c2874351052594f624f8a178
Benegas Joaquín Ezequiel <405312@tecnicatura.frc.utn.edu.ar>
2024-11-14 15:43:04 -0300

    style

--- src/app/domain/moderations/sanction-type/components/appeal-days-form/appeal-days-form.component.ts
+++ src/app/domain/moderations/sanction-type/components/appeal-days-form/appeal-days-form.component.ts
@@ -1,59 +1,64 @@
-import {Component, inject, TemplateRef} from '@angular/core';
-import {ModalDismissReasons, NgbModal} from "@ng-bootstrap/ng-bootstrap";
-import {ConfigurationService} from "../../services/configuration.service";
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {NgClass} from "@angular/common";
+import { Component, inject, TemplateRef } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ConfigurationService } from '../../services/configuration.service';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-appeal-days-form',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    NgClass
-  ],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './appeal-days-form.component.html',
-  styleUrl: './appeal-days-form.component.scss'
+  styleUrl: './appeal-days-form.component.scss',
 })
-export class AppealDaysFormComponent  {
-
-
+export class AppealDaysFormComponent {
   //services
 
   private modalService = inject(NgbModal);
-  private configService = inject(ConfigurationService)
+  private configService = inject(ConfigurationService);
 
   //variables
   closeResult = '';
-  daysToAppeal=0
+  daysToAppeal = 10;
 
   //form
   maxDaysToAppeal: FormGroup = new FormGroup({
-    days: new FormControl(10, [Validators.required,Validators.min(1), Validators.max(360)]),
-  })
+    days: new FormControl(10, [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(360),
+    ]),
+  });
 
   //methods
 
-
   ngOnInit() {
-    this.checkForDays()
+    this.checkForDays();
   }
 
   private checkForDays() {
-    this.configService.getDays().subscribe(days => {
-      this.daysToAppeal = days
-      console.log(this.daysToAppeal)
-    })
+    this.configService.getDays().subscribe((days) => {
+      this.daysToAppeal = days;
+      console.log(this.daysToAppeal);
+    });
   }
 
   open(content: TemplateRef<any>) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-      (result) => {
-        this.closeResult = `Closed with: ${result}`;
-      },
-      (reason) => {
-        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-      },
-    );
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
   private getDismissReason(reason: any): string {
@@ -73,17 +78,16 @@ export class AppealDaysFormComponent  {
       console.log(days);
 
       //todo: el segundo parametro es el id del usuario que hay que cambiar como lo esten usando el grupo 2
-      this.configService.putDays(days,10).subscribe({
+      this.configService.putDays(days, 10).subscribe({
         next: (result) => {
-          console.log('dias nuevos ', days)
-          this.daysToAppeal=result
-          this.modalService.dismissAll()
+          console.log('dias nuevos ', days);
+          this.daysToAppeal = result;
+          this.modalService.dismissAll();
         },
         error: (err) => {
-          console.log('error al actualizar: ', err)
-        }
-      })
+          console.log('error al actualizar: ', err);
+        },
+      });
     }
   }
-
 }
